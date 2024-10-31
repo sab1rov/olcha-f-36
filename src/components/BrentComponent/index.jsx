e02a6c2c54da131138c3c563a95ab278ca1fc7e5
unknown <zohid.zokirivich2000@gmail.com>
2024-10-31 09:29:19 +0500

    bugs fixed

--- src/components/BrentComponent/index.jsx
+++ src/components/BrentComponent/index.jsx
@@ -1,294 +1,334 @@
-import React from 'react'
+import React from "react";
 
 function Brent() {
   return (
-    <section className='brent'>
-
+    <section className="brent">
       <div className="brent__wrap">
-        <div className="cantainer">
-
+        <div className="container">
           <div className="brent__navbar">
             <div className="brent__navbar-cards">
               <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
             <div className="brent__navbar-cards">
-
-            <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+              <div className="brent__navbar-catalog">
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
             <div className="brent__navbar-cards">
-
-            <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+              <div className="brent__navbar-catalog">
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
             <div className="brent__navbar-cards">
-            <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+              <div className="brent__navbar-catalog">
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
             <div className="brent__navbar-cards">
-
-            <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+              <div className="brent__navbar-catalog">
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
             <div className="brent__navbar-cards">
-
-            <div className="brent__navbar-catalog">
-                <img src="./public/Appl.png" alt="Appl" className='brent__navbar-img' />
+              <div className="brent__navbar-catalog">
+                <img
+                  src="./public/Appl.png"
+                  alt="Appl"
+                  className="brent__navbar-img"
+                />
                 <div className="brent__navbar-item">
-                  <a href="#">
-                    BARCAH MAHSULOTLAR
-                  </a>
-
+                  <a href="#">BARCAH MAHSULOTLAR</a>
                 </div>
               </div>
 
               <div className="brent__navbar-wrap">
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/akcustika.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/akcustika.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Akustika</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Akustika</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/media.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/media.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Media Player</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Media Player</span>
+                  </div>
                 </div>
 
                 <div className="brent__navabar-log">
-
-                  <a href="#" className='brent__navbar-logo'>
-                    <img src="./public/vertual.png" alt="akcustika" className=' brent__navbar-image' />
+                  <a href="#" className="brent__navbar-logo">
+                    <img
+                      src="./public/vertual.png"
+                      alt="akcustika"
+                      className=" brent__navbar-image"
+                    />
                   </a>
-                <div className='brent__navbar-akustika'>
-                  <span> Virtual ko'zoynaklar</span>
-                </div>
+                  <div className="brent__navbar-akustika">
+                    <span> Virtual ko'zoynaklar</span>
+                  </div>
                 </div>
               </div>
-
-
             </div>
           </div>
-
         </div>
       </div>
-
     </section>
-  )
+  );
 }
 
-export default Brent
+export default Brent;
